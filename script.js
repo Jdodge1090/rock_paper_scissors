00e7b3d0caa4d54852d72423ccda813589f5b479
Jdodge1090 <j.dodge1090@gmail.com>
2025-03-08 20:26:56 -0500

    Add initial HTML structure and basic game logic
    - Created index.html with basic structure for Rock
         Paper Scissors game
    - Added psuedocode script.js with basic game logic
         for user interaction and result display

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get the computer's choice and store "rock", "paper", or "scissors".
+// Prompt the user to select rock, paper or scissors.
+// Compare the computer choice to the user choice and select a winner. 
+// Declare the winner.
+// The game will be played to best 3 out of 5 rounds. It will repeat until humanScore or computerScore = 3. 
+// End the game letting the user know who won the game.
