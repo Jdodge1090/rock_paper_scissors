97258baebbe1d013153bf21bfa558fc0da69c0d3
Jdodge1090 <j.dodge1090@gmail.com>
2025-03-09 20:10:18 -0400

    Add functionality to get choices
    - Add function to get the user selection
    - Add function to get a random number for computer
        selection.

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+function getUserChoice() {
+    return prompt("Select Rock, Paper or Scissors: ").toLowerCase();
+}
+
+let userChoice = getUserChoice();
+console.log(userChoice);
+
+/*
+ * Function to get the computer's choice for rock, paper, scissors.
+ * Returns a random number between 0 and 2.
+ */
+
+function getComputerChoice() {
+    min = Math.ceil(0);
+    max = Math.floor(2);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let computerChoice = getComputerChoice(0, 2);
+console.log(computerChoice);
+
 // Get the computer's choice and store "rock", "paper", or "scissors".
 // Prompt the user to select rock, paper or scissors.
 // Compare the computer choice to the user choice and select a winner. 
